41909b886b2d6c63d8b8fc3e0485ed44f4d2d75c
NoName-2028 <4idiotas2028@gmail.com>
2026-08-16 14:33:24 +0530

    Update UsedCar_Dataset.py

--- UsedCar_Dataset.py
+++ UsedCar_Dataset.py
@@ -9,7 +9,7 @@ from sklearn.model_selection import train_test_split
 df = pd.read_csv("Used_Car_Dataset - Used_Car_Dataset.csv")
 
 # 2. Select numeric feature (X) and target variable (y)
-X = df[["Engine_Size_cc"]]  # Feature: Engine size [Using third bracket for defining it as a 2D dataset]
+X = df[["Engine_Size_cc"]]  # Feature: Engine size [Using two third bracket for defining it as a 2D dataset]
 y = df["Price_INR"]  # Target: Price [Predict or Test Data]
 
 # 3. Split into 70% Training data and 30% Testing data(As given instruction) ------
